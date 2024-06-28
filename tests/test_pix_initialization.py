e898d5ce8d732e8adc1f43e70729b547c74c137d
Yulia Magonya <ymagonia@gmail.com>
2024-06-29 00:22:06 +0300

    pytest ver2

--- tests/test_pix_initialization.py
+++ tests/test_pix_initialization.py
@@ -1,15 +1,14 @@
 import pytest
-
 from staff import pxl
 
 
 @pytest.mark.parametrize("red, green, blue, expected_error", [
     (100, 150, 200, None),
     (256, 150, 200, ValueError),
-    (100, -10, 200, ValueError),
+    (100, -1, 200, ValueError),
     (100, 150, 300, ValueError)
 ], ids=["correct values", "red is incorrect", "green is incorrect", "blue is incorrect"])
-def test_addition(red, green, blue, expected_error):
+def test_pix_initialization(red, green, blue, expected_error):
     if expected_error:
         with pytest.raises(expected_error):
             pxl.Pixel(red, green, blue)
@@ -18,5 +17,3 @@ def test_addition(red, green, blue, expected_error):
         assert pixel.red == red
         assert pixel.green == green
         assert pixel.blue == blue
-
-
